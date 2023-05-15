493d6675ad98bc2e39be8757902677756acdc40c
GoninDS <luisdi2509@gmail.com>
2023-05-15 08:16:42 -0600

    Fixed issue with private_data_vector

--- threads/main.cpp
+++ threads/main.cpp
@@ -19,11 +19,10 @@ double handle_threads(int number_amount, int& thread_count) {
   // Vector containing the threads
   std::vector<std::thread> threads;
   // Private data for each thread
-  std::vector<private_data> private_data_vector;
+  std::vector<private_data> private_data_vector(thread_count);
   // Creates the threads
   for (int i = 0; i < thread_count; ++i) {
     // Create the private data
-    private_data_vector.emplace_back();
     private_data_vector[i].thread_number = i;
     private_data_vector[i].thread_count = thread_count;
     // Create the thread
@@ -39,29 +38,37 @@ double handle_threads(int number_amount, int& thread_count) {
 }
 
 void generate_points(int number_amount, private_data* private_data) {
-  // Calculate the respective block
-  int start = private_data->thread_number * (number_amount / private_data->thread_count) +
-      MIN(private_data->thread_number, number_amount % private_data->thread_count);
-  int end = private_data->thread_number + 1 * (number_amount / private_data->thread_count) +
-      MIN(private_data->thread_number + 1, number_amount % private_data->thread_count);
-  // Create the default random engine generator
-  std::default_random_engine generator;
-  // Set the distribution in the range [0,1[
-  std::uniform_real_distribution<double> distribution(0.0,1.0);
-  double x_value = 0.0;
-  double y_value = 0.0;
-  bool in_circle = false;
-  for (int i = start; i < end; ++i) {
-    // Generate the random points
-    x_value = distribution(generator);
-    y_value = distribution(generator);
-    // Calculate if it is in the circle
-    in_circle = calculate_position(x_value, y_value);
-    // Add to count
-    if (in_circle) {
-      ++private_data->total_in_circle;
-    }
-  }
+
+  std::string greet = "Hello from generate points #";
+  greet += std::to_string(private_data->thread_number);
+  greet += " of ";
+  greet += std::to_string(private_data->thread_count);
+  greet += "\n";
+  std::cout << greet;
+
+  // // Calculate the respective block
+  // int start = private_data->thread_number * (number_amount / private_data->thread_count) +
+  //     MIN(private_data->thread_number, number_amount % private_data->thread_count);
+  // int end = private_data->thread_number + 1 * (number_amount / private_data->thread_count) +
+  //     MIN(private_data->thread_number + 1, number_amount % private_data->thread_count);
+  // // Create the default random engine generator
+  // std::default_random_engine generator;
+  // // Set the distribution in the range [0,1[
+  // std::uniform_real_distribution<double> distribution(0.0,1.0);
+  // double x_value = 0.0;
+  // double y_value = 0.0;
+  // bool in_circle = false;
+  // for (int i = start; i < end; ++i) {
+  //   // Generate the random points
+  //   x_value = distribution(generator);
+  //   y_value = distribution(generator);
+  //   // Calculate if it is in the circle
+  //   in_circle = calculate_position(x_value, y_value);
+  //   // Add to count
+  //   if (in_circle) {
+  //     ++private_data->total_in_circle;
+  //   }
+  // }
 }
 
 bool calculate_position(double& x_value, double& y_value) {
