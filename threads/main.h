fa48e777518f364b36122f087f8515f31153a5b5
GoninDS <luisdi2509@gmail.com>
2023-05-15 22:02:04 -0600

    Documentation

--- threads/main.h
+++ threads/main.h
@@ -17,14 +17,15 @@ struct private_data {
 /**
  * @brief Creates the threads, executes them and joins them
  * 
- * @param thread_cout The amount of threads
+ * @param number_amount The amount of points to be created
+ * @param thread_count The amount of threads
  */
 double handle_threads(int number_amount, int& thread_count);
 /**
  * @brief Generates points from [0,1[ with a standard distribution
  * 
- * @param points Vector that contains if the points are in or outside of the circle
  * @param number_amount The amount of numbers used for the simulation
+ * @param private_data Contains the private data for the thread
  */
 void generate_points(int number_amount, private_data* private_data);
 /**
@@ -40,6 +41,7 @@ bool calculate_position(double& x_value, double& y_value);
  * @brief Approximates pi with the amount of points calculated in the circle
  * 
  * @param points Contains the vector that contains if the points are in or outside of the circle
+ * @param total_in_circle Contains the amount of points inside the circle
  * @return double The answer with the approximated pi
  */
 double approximate_pi(int& number_amount, double& total_in_circle);
